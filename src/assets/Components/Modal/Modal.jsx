e0ce41fc51ea36a10b918736fbac6944c3b70a94
Rovshan Alizada <148777319+rovshanalizada7@users.noreply.github.com>
2025-01-01 20:30:49 +0400

    some errors fixed in modal
    done

--- src/assets/Components/Modal/Modal.jsx
+++ src/assets/Components/Modal/Modal.jsx
@@ -9,17 +9,24 @@ const Modal = ({ show, onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-<<<<<<< Updated upstream
         <button className="close-button" onClick={onClose}>×</button>
-        <h3 className='h3'>Aylıq ödəniş</h3>
-=======
-        <button className="close-button" onClick={onClose}>X</button>
-        <h3>Aylıq ödəniş</h3>
->>>>>>> Stashed changes
+        <h3 className="modal-title">Aylıq ödəniş</h3>
         <div className="payment-options">
-          <img style={{width:"174px",height:"49px"}} src="https://irshad.az/images/million.jpeg" alt="Million" />
-          <img style={{width:"174px",height:"46px"}} src="https://irshad.az/images/epul.jpeg" alt="Epul" />
-          <img style={{width:"173px",height:"95px"}} src="https://irshad.az/images/hesabaz.jpeg" alt="Hesab" />
+          <img
+            style={{ width: '174px', height: '49px' }}
+            src="https://irshad.az/images/million.jpeg"
+            alt="Million"
+          />
+          <img
+            style={{ width: '174px', height: '46px' }}
+            src="https://irshad.az/images/epul.jpeg"
+            alt="Epul"
+          />
+          <img
+            style={{ width: '173px', height: '95px' }}
+            src="https://irshad.az/images/hesabaz.jpeg"
+            alt="Hesab"
+          />
         </div>
       </div>
     </div>
